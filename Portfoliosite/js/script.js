cc3536cd76f91a8cca58b98f2cebc17d51413624
JimiMorris <jimiart@gmail.com>
2016-07-09 19:04:27 -0500

    Update script.js

--- Portfoliosite/js/script.js
+++ Portfoliosite/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $('ul.first').bsPhotoGallery({
-    "classes" : "col-lg-2 col-md-4 col-sm-3 col-xs-4 col-xxs-12",
+    "classes" : "col-lg-3 col-md-4 col-sm-6 col-xs-12 col-xxs-12",
     "hasModal" : true
   });
 });
